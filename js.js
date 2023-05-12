c899d0a6c3a28326a320077d167bbf9b86fc0168
Saif Dinne Gtari <131154810+SaifGtar@users.noreply.github.com>
2023-05-12 14:01:48 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .container .nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+});
